75f234324f9c7730cdec344f00a6bf453ca03e19
Adem <guedriadem@gmail.com>
2025-07-07 00:24:14 +0100

    fix: update route to use IndexController instead of TestController

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\TestController;
 use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[TestController::class,'index']);
+Route::get('/',[IndexController::class,'index']);
 Route::get('/tweet/view',[TweetController::class,'view']);
 Route::get('/login',[LoginController::class,'create']);
 Route::get('/register',[RegisterController::class,'create']);
