9cba778df2aac59baf4c6188e0e8228729559eeb
Adem <guedriadem@gmail.com>
2025-07-07 00:23:26 +0100

    feat: create index view with trend components

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app>
+    <x-trend></x-trend>
+    <x-trend></x-trend>
+    <x-trend></x-trend>
+    <x-trend></x-trend>
+    <x-trend></x-trend>
+    <x-trend></x-trend>
+</x-layouts.app>
