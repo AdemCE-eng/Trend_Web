e7735da22e0651fc9922b6214f9ef583741b2354
Adem <guedriadem@gmail.com>
2025-07-14 22:18:01 +0100

    refactor: update index method to return view instead of user data

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    function index() {
-        return User::with("tweets")->get();
-}
+    function index()
+    {
+        return view('index');
+    }
 }
