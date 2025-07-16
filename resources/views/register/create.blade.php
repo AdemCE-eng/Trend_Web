04c7c993cd89fc74d8831a742b7c2098db417deb
Adem <guedriadem@gmail.com>
2025-07-16 16:20:41 +0100

    refactor: enhance registration form layout and input accessibility

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -2,53 +2,70 @@
   <div class="drop-shadow-2xl flex h-auto min-h-screen items-center justify-center overflow-x-hidden py-10">
     <div class="relative flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <div class="bg-base-100 shadow-base-300/20 z-1 w-full space-y-6 rounded-xl p-6 shadow-md sm:min-w-md lg:p-8">
-        <a href="{{ route('home') }}" class="flex items-center gap-3">
+        <a href="{{ route('home') }}" class="flex items-center gap-3 pl-3">
           <img src="/images/Trends_Logo.png" class="size-8 rounded-md" alt="logo">
           <h2 class="text-base-content text-xl font-bold">Trends</h2>
         </a>
-        <div>
+        <div class="pl-3">
           <h3 class="text-base-content mb-1.5 text-2xl font-semibold">Sign Up to Trends</h3>
         </div>
-        <div class="space-y-4">
-          <form class="mb-4 space-y-4">
-            <div>
-              <label class="label-text" for="name">Username*</label>
-              <input type="text" 
-                     placeholder="Enter your username" 
-                     class="input" 
-                     id="name" 
-                     name="name" 
-                     required 
-                     minlength="3" 
-                     maxlength="20"
-                     pattern="^[a-zA-Z][a-zA-Z0-9_]*$"
-                     value="{{ old('name') }}"
-                     title="Username must be 3-20 characters, start with a letter, and contain only letters, numbers, or underscores (no spaces allowed)"/>
+        <div class="space-y-4 pl-3">
+          <form method="post" class="mb-4 space-y-4">
+            @csrf
+            <div class="input">
+              <span class="icon-[tabler--user] text-base-content/80 my-auto size-5 shrink-0"></span>
+              <div class="input-floating grow">
+                <input
+                  required
+                  type="text"
+                  placeholder="Enter your username"
+                  class="ps-3"
+                  id="name"
+                  name="name"
+                  maxLength="20"
+                  minlength="3"
+                  pattern="^[a-zA-Z][a-zA-Z0-9_]*$"
+                  value="{{ old('name') }}"
+                  title="Username must be 3-20 characters, start with a letter, and contain only letters, numbers, or underscores (no spaces allowed)"
+                />
+                <label class="input-floating-label" for="name">Username</label>
+              </div>
+            </div>
+            <div class="input">
+              <span class="icon-[tabler--mail] text-base-content/80 my-auto size-5 shrink-0"></span>
+              <div class="input-floating grow">
+                <input
+                  required
+                  type="email"
+                  placeholder="Enter your email address"
+                  class="ps-3"
+                  id="email"
+                  name="email"
+                  maxLength="255"
+                  value="{{ old('email') }}"
+                  title="Please enter a valid email address"
+                />
+                <label class="input-floating-label" for="email">Email address</label>
+              </div>
             </div>
             <div>
-              <label class="label-text" for="email">Email address*</label>
-              <input type="email" 
-                     placeholder="Enter your email address" 
-                     class="input" 
-                     id="email" 
-                     name="email" 
-                     required 
-                     maxlength="255"
-                     value="{{ old('email') }}"
-                     title="Please enter a valid email address"/>
-            </div>
-            <div>
-              <label class="label-text" for="password">Password*</label>
               <div class="input">
-                <input id="password" 
-                       type="password" 
-                       name="password" 
-                       placeholder="············" 
-                       required 
-                       minlength="8" 
-                       maxlength="255"
-                       pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                       title="Password must be at least 8 characters with uppercase, lowercase, number, and special character" />
+                <span class="icon-[tabler--lock] text-base-content/80 my-auto size-5 shrink-0"></span>
+                <div class="input-floating grow">
+                  <input
+                    required
+                    type="password"
+                    placeholder="············"
+                    class="ps-3"
+                    id="password"
+                    name="password"
+                    maxLength="255"
+                    minlength="8"
+                    pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                    title="Password must be at least 8 characters with uppercase, lowercase, number, and special character"
+                  />
+                  <label class="input-floating-label" for="password">Password</label>
+                </div>
                 <button type="button" data-toggle-password='{ "target": "#password" }' class="block cursor-pointer"
                   aria-label="Toggle password visibility">
                   <span class="icon-[tabler--eye] password-active:block hidden size-5 shrink-0"></span>
@@ -95,22 +112,26 @@
                 </ul>
               </div>
             </div>
-            <div>
-              <label class="label-text" for="password_confirmation">Confirm Password*</label>
-              <div class="input">
-                <input id="password_confirmation" 
-                       type="password" 
-                       name="password_confirmation" 
-                       placeholder="············" 
-                       required 
-                       minlength="8" 
-                       maxlength="255" />
-                <button type="button" data-toggle-password='{ "target": "#password_confirmation" }'
-                  class="block cursor-pointer" aria-label="Toggle password confirmation visibility">
-                  <span class="icon-[tabler--eye] password-active:block hidden size-5 shrink-0"></span>
-                  <span class="icon-[tabler--eye-off] password-active:hidden block size-5 shrink-0"></span>
-                </button>
+            <div class="input">
+              <span class="icon-[tabler--lock-check] text-base-content/80 my-auto size-5 shrink-0"></span>
+              <div class="input-floating grow">
+                <input
+                  required
+                  type="password"
+                  placeholder="············"
+                  class="ps-3"
+                  id="password_confirmation"
+                  name="password_confirmation"
+                  maxLength="255"
+                  minlength="8"
+                />
+                <label class="input-floating-label" for="password_confirmation">Confirm Password</label>
               </div>
+              <button type="button" data-toggle-password='{ "target": "#password_confirmation" }'
+                class="block cursor-pointer" aria-label="Toggle password confirmation visibility">
+                <span class="icon-[tabler--eye] password-active:block hidden size-5 shrink-0"></span>
+                <span class="icon-[tabler--eye-off] password-active:hidden block size-5 shrink-0"></span>
+              </button>
             </div>
             <div class="flex items-center gap-2">
               <input type="checkbox" 
