5afc6b26c5ccda21174ed391ede963aba9df0a06
Adem <guedriadem@gmail.com>
2025-07-18 20:28:34 +0100

    fix: remove unnecessary whitespace and ensure consistent formatting in routes

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,14 @@ Route::get('/', [IndexController::class, 'index'])
 
 Route::get('/tweet/view', [TweetController::class, 'view'])
     ->name('tweet.view');
-    
+
 Route::get('/register', [RegisterController::class, 'create'])
     ->name('register');
-
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'create'])
     ->name('login');
+Route::post('/login', [LoginController::class,'store']);
 
 Route::post('/logout', LogoutController::class)
     ->name('logout');
