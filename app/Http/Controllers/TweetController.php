d2d6af9d7725c36cb1099c97bee775e52870aba2
Adem <guedriadem@gmail.com>
2025-07-21 17:54:23 +0100

    feat: refactor TweetController to improve tweet retrieval and organization

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTweetRequest;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TweetController extends Controller
 {
-    function view(){
+    function index()
+    {
+        $tweets = Tweet::all();
+        return view('index', compact('tweets'));
+    }
+
+    function view()
+    {
         return view("/tweet/view");
+    }
+
+    function store(StoreTweetRequest $request)
+    {
+        Auth::user()->tweets()->create($request->validated());
+        return redirect()->back();
     }
 }
