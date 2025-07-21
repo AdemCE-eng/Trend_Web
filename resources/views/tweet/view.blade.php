f02dc91c7cd2288d71fb662dd372c61176a5720a
Adem <guedriadem@gmail.com>
2025-07-21 22:44:13 +0100

    fix: update tweet component rendering in view for proper data binding

--- resources/views/tweet/view.blade.php
+++ resources/views/tweet/view.blade.php
@@ -1,11 +1,11 @@
 <x-layouts.app>
-    <x-trend></x-trend>
-    <div class="ms-4 ps-2 border-s-2 border-s-black/60">
-        <x-trend></x-trend>
+    <x-trend :tweet="$tweet"></x-trend>
+    <!-- <div class="ms-4 ps-2 border-s-2 border-s-black/60">
+        <x-trend :tweet="$tweet"></x-trend>
         <div class="ms-2 ps-2 border-s-2 border-base-content/50">
-            <x-trend></x-trend>
+            <x-trend :tweet="$tweet"></x-trend>
         </div>
-        <x-trend></x-trend>
-        <x-trend></x-trend>
-    </div>
+        <x-trend :tweet="$tweet"></x-trend>
+        <x-trend :tweet="$tweet"></x-trend>
+    </div> -->
 </x-layouts.app>
