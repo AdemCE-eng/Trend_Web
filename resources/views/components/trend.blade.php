3c5b62dc0e88bb6d7d5e15d9dcf036134509be43
Adem <guedriadem@gmail.com>
2025-07-21 17:54:48 +0100

    feat: update tweet component to display user avatar and name dynamically

--- resources/views/components/trend.blade.php
+++ resources/views/components/trend.blade.php
@@ -1,18 +1,19 @@
+@props([
+    'tweet', 
+])
 <a href="{{ route('tweet.view') }}">
     <div class="card card-border shadow-none mt-4 bg-green-30">
         <div class="card-body">
             <div class="flex items-center mb-3">
                 <div class="size-10 rounded-full mr-3">
-                    <img src="https://cdn.flyonui.com/fy-assets/avatar/avatar-1.png" alt="avatar"
+                    <img src="{{ $tweet->user->avatar_url }}" alt="avatar"
                         class="w-full h-full rounded-full object-cover" />
                 </div>
                 <div class="font-medium text-gray-700">
-                    Guest
+                    {{ $tweet->user->name }}
                 </div>
             </div>
-            <p class="mb-4 ml-13">Discover the features and benefits that our service offers. Enhance your experience
-                with our
-                user-friendly platform designed to meet all your needs.</p>
+            <p class="mb-4 ml-10 mr-10 break-words overflow-hidden">{{ $tweet->content }}</p>
             <div class="flex gap-4 ml-13">
                 <button class="btn-sm">
                     <span class="icon-[tabler--message-circle] size-4"></span>
