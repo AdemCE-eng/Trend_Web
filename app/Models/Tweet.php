4bca9b11d283efedcb637c5ad77517da19b07dd6
Adem <guedriadem@gmail.com>
2025-07-23 16:55:31 +0100

    feat: add relationships for retweets and replies in Tweet model

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -11,10 +11,47 @@ class Tweet extends Model
     
     protected $fillable = [
         'content',
+        'base_tweet_id',
+        'parent_tweet_id'
     ];
     
+    /**
+     * Get the user that owns the tweet.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+    
+    /**
+     * Get the base tweet that this tweet is retweeting.
+     */
+    public function baseTweet()
+    {
+        return $this->belongsTo(Tweet::class, 'base_tweet_id');
+    }
+    
+    /**
+     * Get all retweets of this tweet.
+     */
+    public function retweets()
+    {
+        return $this->hasMany(Tweet::class, 'base_tweet_id');
+    }
+    
+    /**
+     * Get the parent tweet that this tweet is replying to.
+     */
+    public function parentTweet()
+    {
+        return $this->belongsTo(Tweet::class, 'parent_tweet_id');
+    }
+    
+    /**
+     * Get all replies to this tweet.
+     */
+    public function replies()
+    {
+        return $this->hasMany(Tweet::class, 'parent_tweet_id');
+    } 
 }
