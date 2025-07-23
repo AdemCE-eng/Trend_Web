25877cf37a6bfe3c7bcec63cff8f25403cfaafbe
Adem <guedriadem@gmail.com>
2025-07-23 22:11:14 +0100

    feat: simplify tweet view by removing redundant trend components

--- resources/views/tweet/view.blade.php
+++ resources/views/tweet/view.blade.php
@@ -1,11 +1,3 @@
 <x-layouts.app>
-    <x-trend :tweet="$tweet"></x-trend>
-    <!-- <div class="ms-4 ps-2 border-s-2 border-s-black/60">
-        <x-trend :tweet="$tweet"></x-trend>
-        <div class="ms-2 ps-2 border-s-2 border-base-content/50">
-            <x-trend :tweet="$tweet"></x-trend>
-        </div>
-        <x-trend :tweet="$tweet"></x-trend>
-        <x-trend :tweet="$tweet"></x-trend>
-    </div> -->
+    <x-trend :tweet="$tweet" :depth="0"></x-trend>
 </x-layouts.app>
